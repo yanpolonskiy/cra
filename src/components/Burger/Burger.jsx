ce6fefd6b927b2cd21bd5c102974973a46711b0b
Sun1ive <sunliveua@gmail.com>
2018-04-04 13:21:52 +0300

    upd

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,9 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient.jsx';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(ingr => [
-    ...Array(props.ingredients[ingr]),
-  ].map((_, index) => <BurgerIngredient key={ingr + index} type={ingr} />))
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingr =>
+      [...Array(props.ingredients[ingr])].map((_, index) => (
+        <BurgerIngredient key={ingr + index} type={ingr} />
+      )),
+    )
+    .reduce((arr, el) => arr.concat(el), []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
